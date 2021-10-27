bf373327544784f507c0f27cc2d40defd9faffb5
khalid <khalidmiah@hotmail.co.uk>
2021-10-27 12:52:35 +0100

    added temp up and down methods

---
+++ spec/thermostatSpec.js
@@ -0,0 +1,25 @@
+'use strict';
+
+describe('Thermostat', () => {
+
+  let thermostat;
+
+  beforeEach(() => {
+    thermostat = new Thermostat();
+  });
+
+  it('starts at 20 degrees', () => {
+    expect(thermostat.getCurrentTemperature()).toEqual(20);
+  });
+
+
+  it('increases in temperature with up()', () => {
+    thermostat.up();
+    expect(thermostat.getCurrentTemperature()).toEqual(21);
+  });
+
+  it('decreases in temperature with down()', () => {
+    thermostat.down();
+    expect(thermostat.getCurrentTemperature()).toEqual(19);
+  });
+});
