b454f31e6ac103c800ee594854ad962e02d59822
khalid <khalidmiah@hotmail.co.uk>
2021-10-28 10:55:10 +0100

    DOM created

---
+++ src/interface.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", () => {
+   
+  
+    const updateTemperature = () => {
+      document.querySelector('#temperature').innerText = thermostat.getCurrentTemperature();
+      document.querySelector('#temperature').className = thermostat.energyUsage();
+    }
+
+    const thermostat = new Thermostat();
+    updateTemperature();
+  
+    document.querySelector('#temperature').addEventListener('click', () => {
+      getCurrentTemperature()
+      updateTemperature();
+    });
+
+    document.querySelector('#temperature-up').addEventListener('click', () => {
+      thermostat.up();
+      updateTemperature();
+    });
+  
+    document.querySelector('#temperature-down').addEventListener('click', () => {
+      thermostat.down();
+      updateTemperature();
+    });
+    document.querySelector('#temperature-reset').addEventListener('click', () => {
+        thermostat.resetTemperature();
+        updateTemperature();
+      });
+      document.querySelector('#powersaving-on').addEventListener('click', () => {
+        thermostat.switchPowerSavingModeOn();
+        document.querySelector('#power-saving-status').innerText = 'on!';
+        updateTemperature();
+      })
+
+
+        document.querySelector('#powersaving-off').addEventListener('click', () => {
+        thermostat.switchPowerSavingModeOff();
+        document.querySelector('#power-saving-status').innerText = 'off!';
+        updateTemperature();
+        })
+});    
+
